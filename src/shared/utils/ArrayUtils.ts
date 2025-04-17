ab38eedf313ffea6f5f7bbc5738f50c5e372779e
sanghunkim20 <kow5562@gmail.com>
2025-04-17 09:48:45 +0900

    chore: 주석 수정

--- src/shared/utils/ArrayUtils.ts
+++ src/shared/utils/ArrayUtils.ts
@@ -4,15 +4,23 @@
  * @param array - 중복 제거할 배열
  * @returns 중복 없는 배열
  */
+
+/**
+ * removeDuplicates JavaScript의 Set을 사용하여 배열에서 중복된 요소를 제거
+ */
 export const removeDuplicates = <T>(array: T[]): T[] => {
   return Array.from(new Set(array));
 };
 
 /**
- * flattenArray 함수는 다차원 배열을 1차원 배열로 평탄화합니다.
+ * flattenArray 함수는 다차원 배열을 1차원 배열로 평탄화
  *
  * @param arr - 다차원 배열
  * @returns 평탄화된 1차원 배열
+ */
+
+/**
+ * flattenArray 함수는 재귀적 방법을 통해 다차원 배열을 1차원으로 평탄화
  */
 export const flattenArray = (arr: any[]): any[] => {
   return arr.reduce(
