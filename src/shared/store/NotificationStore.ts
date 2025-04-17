45319cd7724a5b74861f4765668066801e75dc55
sanghunkim20 <kow5562@gmail.com>
2025-04-17 17:12:26 +0900

    fix: 유저, 알림 상태 관리 수정
    NotificationStore
    - 알림 API Response을 반영하여 코드 수정
    
    UserStore
    - jwt토큰을 반영하여 코드 수정

--- src/shared/store/NotificationStore.ts
+++ src/shared/store/NotificationStore.ts
@@ -1,15 +1,34 @@
 import { create } from 'zustand';
 
-// 알림 상태 관리 타입 정의
+// API 로부터 받아오는 알림 객체 타입 정의
+export interface Notification {
+  alarmDetailId: number;
+  alarmId: number;
+  content: string;
+  language: string;
+}
+
+// Zustand를 이용한 알림 상태 관리 타입 정의
 interface NotificationStore {
-  notifications: string[];
-  addNotification: (message: string) => void;
+  notifications: Notification[];
+  // API 호출 결과로 한 번에 세팅할 때
+  setNotifications: (notifications: Notification[]) => void;
+  // 실시간 알림 추가
+  addNotification: (notification: Notification) => void;
+  // 전체 알림 초기화
   clearNotifications: () => void;
 }
 
-// Zustand를 이용해 알림 상태(store) 생성
+// NotificationStore 생성
 export const useNotificationStore = create<NotificationStore>(set => ({
   notifications: [],
-  addNotification: message => set(state => ({ notifications: [...state.notifications, message] })),
+
+  setNotifications: notifications => set({ notifications }),
+
+  addNotification: notification =>
+    set(state => ({
+      notifications: [...state.notifications, notification],
+    })),
+
   clearNotifications: () => set({ notifications: [] }),
 }));
